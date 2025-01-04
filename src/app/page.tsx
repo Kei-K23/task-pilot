50054f114b5eac0eac3ac620a149703326d3edf8
Kei-K23 <keiksl2301@gmail.com>
2025-01-04 12:34:59 +0630

    feat: add user button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-"use client";
+import UserButton from "@/features/auth/components/user-button";
 
-import { Button } from "@/components/ui/button";
-import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-export default function Home() {
-  const router = useRouter();
-  const { mutate } = useLogout();
-  const { data, isLoading } = useCurrent();
-
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data]);
-
+export default async function Home() {
   return (
     <div>
-      <p>Protected route</p>
-      <p>{data?.name}</p>
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { useCurrent } from "../api/use-current";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut } from "lucide-react";
+import { useLogout } from "../api/use-logout";
+
+export default function UserButton() {
+  const { data: user, isLoading } = useCurrent();
+  const { mutate: logout, isPending } = useLogout();
+
+  if (isLoading) {
+    return (
+      <Skeleton className="size-10 rounded-full bg-neutral-300 hover:opacity-75 transition-all" />
+    );
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  const { email, name } = user;
+  const fallbackName =
+    name?.charAt(0).toUpperCase() || email?.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar className="size-10 border border-neutral-300 transition-all hover:opacity-75 cursor-pointer">
+          <AvatarFallback className="bg-neutral-200 font-medium">
+            {fallbackName}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        side="bottom"
+        className="w-60"
+        sideOffset={10}
+      >
+        <div className="flex items-center justify-center flex-col">
+          <Avatar className="size-16 border border-neutral-300 transition-all hover:opacity-75 cursor-pointer">
+            <AvatarFallback className="bg-neutral-200 font-medium text-2xl">
+              {fallbackName}
+            </AvatarFallback>
+          </Avatar>
+          <span className="mt-2">{name}</span>
+          <span className="text-sm text-muted-foreground">{email}</span>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => logout()}
+          disabled={isPending}
+          className="flex items-center justify-center bg-red-500 text-white focus:bg-red-500/80 focus:text-white cursor-pointer"
+        >
+          <LogOut /> Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
