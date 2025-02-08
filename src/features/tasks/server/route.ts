2b14d613aa2d31893d54cef56f8cac496faff5fc
Kei-K23 <keiksl2301@gmail.com>
2025-02-08 21:55:53 +0630

    feat: add task list api route handler to get all task

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -2,77 +2,196 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { taskCreateSchema } from "../schemas";
-import { DATABASE_ID, TASKS_ID } from "@/config";
+import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID } from "@/config";
 import { ID, Query } from "node-appwrite";
 import { getMember } from "@/features/members/queries";
+import { z } from "zod";
+import { Task, TASK_STATUS } from "../type";
+import { createAdminClient } from "@/lib/appwrite";
 
-const app = new Hono().post(
-  "/",
-  zValidator("json", taskCreateSchema),
-  sessionMiddleware,
-  async (c) => {
-    const {
-      workspaceId,
-      name,
-      projectId,
-      assigneeId,
-      status,
-      dueDate,
-      description,
-    } = c.req.valid("json");
-    const databases = c.get("databases");
-    const user = c.get("user");
+const app = new Hono()
+  .get(
+    "/",
+    zValidator(
+      "query",
+      z.object({
+        workspaceId: z.string(),
+        projectId: z.string().nullish(),
+        assigneeId: z.string().nullish(),
+        status: z.nativeEnum(TASK_STATUS).nullish(),
+        dueDate: z.string().nullish(),
+        search: z.string().nullish(),
+      })
+    ),
+    sessionMiddleware,
+    async (c) => {
+      const { users } = await createAdminClient();
+      const databases = c.get("databases");
+      const user = c.get("user");
 
-    const member = getMember(databases, workspaceId, user.$id);
+      const { workspaceId, projectId, assigneeId, status, dueDate, search } =
+        c.req.valid("query");
 
-    if (!member) {
-      return c.json(
-        {
-          success: false,
-          message: "Unauthorized",
-          data: null,
-        },
-        401
-      );
-    }
+      const member = await getMember(databases, workspaceId, user.$id);
 
-    const highestPositionTask = await databases.listDocuments(
-      DATABASE_ID,
-      TASKS_ID,
-      [
-        Query.equal("status", status),
+      if (!member) {
+        return c.json(
+          {
+            success: false,
+            message: "Unauthorized",
+            data: null,
+          },
+          401
+        );
+      }
+
+      const query = [
         Query.equal("workspaceId", workspaceId),
-        Query.equal("projectId", projectId),
-        Query.orderAsc("position"),
-        Query.limit(1),
-      ]
-    );
+        Query.orderDesc("$createdAt"),
+      ];
 
-    const newPosition =
-      highestPositionTask.total === 0
-        ? 1000
-        : highestPositionTask.documents[0].position + 1000;
+      if (projectId) {
+        query.push(Query.equal("projectId", projectId));
+      }
 
-    const task = await databases.createDocument(
-      DATABASE_ID,
-      TASKS_ID,
-      ID.unique(),
-      {
-        name,
+      if (assigneeId) {
+        query.push(Query.equal("assigneeId", assigneeId));
+      }
+
+      if (status) {
+        query.push(Query.equal("status", status));
+      }
+
+      if (dueDate) {
+        query.push(Query.equal("dueDate", dueDate));
+      }
+
+      if (search) {
+        query.push(Query.search("name", search));
+      }
+
+      // Get the tasks according to query filter
+      const tasks = await databases.listDocuments<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        query
+      );
+
+      const projectIds = tasks.documents.map((task) => task.projectId);
+      const assigneeIds = tasks.documents.map((task) => task.assigneeId);
+
+      const projects = await databases.listDocuments(
+        DATABASE_ID,
+        PROJECTS_ID,
+        projectIds.length > 0 ? [Query.contains("$id", projectIds)] : []
+      );
+
+      const members = await databases.listDocuments(
+        DATABASE_ID,
+        MEMBERS_ID,
+        assigneeIds.length > 0 ? [Query.contains("$id", assigneeIds)] : []
+      );
+
+      const assignees = await Promise.all(
+        members.documents.map(async (member) => {
+          const user = await users.get(member.userId);
+
+          return {
+            ...member,
+            name: user.name,
+            email: user.email,
+          };
+        })
+      );
+
+      const populatedTasks = tasks.documents.map((task) => {
+        const project = projects.documents.filter(
+          (project) => project.$id === task.projectId
+        );
+
+        const assignee = assignees.filter(
+          (assignee) => assignee.$id === task.assigneeId
+        );
+
+        return {
+          ...task,
+          project,
+          assignee,
+        };
+      });
+
+      return c.json({
+        data: populatedTasks,
+      });
+    }
+  )
+  .post(
+    "/",
+    zValidator("json", taskCreateSchema),
+    sessionMiddleware,
+    async (c) => {
+      const {
         workspaceId,
+        name,
         projectId,
         assigneeId,
-        description,
-        dueDate,
         status,
-        position: newPosition,
-      }
-    );
+        dueDate,
+        description,
+      } = c.req.valid("json");
+      const databases = c.get("databases");
+      const user = c.get("user");
 
-    return c.json({
-      data: task,
-    });
-  }
-);
+      const member = getMember(databases, workspaceId, user.$id);
+
+      if (!member) {
+        return c.json(
+          {
+            success: false,
+            message: "Unauthorized",
+            data: null,
+          },
+          401
+        );
+      }
+
+      const highestPositionTask = await databases.listDocuments(
+        DATABASE_ID,
+        TASKS_ID,
+        [
+          Query.equal("status", status),
+          Query.equal("workspaceId", workspaceId),
+          Query.equal("projectId", projectId),
+          Query.orderAsc("position"),
+          Query.limit(1),
+        ]
+      );
+
+      const newPosition =
+        highestPositionTask.total === 0
+          ? 1000
+          : highestPositionTask.documents[0].position + 1000;
+
+      const task = await databases.createDocument(
+        DATABASE_ID,
+        TASKS_ID,
+        ID.unique(),
+        {
+          name,
+          workspaceId,
+          projectId,
+          assigneeId,
+          description,
+          dueDate,
+          status,
+          position: newPosition,
+        }
+      );
+
+      return c.json({
+        data: task,
+      });
+    }
+  );
 
 export default app;
--- src/features/tasks/type.ts
+++ src/features/tasks/type.ts
@@ -1,3 +1,5 @@
+import { Models } from "node-appwrite";
+
 export enum TASK_STATUS {
   BACKLOG = "BACKLOG",
   IN_PROGRESS = "IN_PROGRESS",
@@ -5,3 +7,13 @@ export enum TASK_STATUS {
   DONE = "DONE",
   IN_REVIEW = "IN_REVIEW",
 }
+
+export type Task = Models.Document & {
+  name: string;
+  workspaceId: string;
+  projectId: string;
+  assigneeId: string;
+  status: string;
+  dueDate: string;
+  description: string;
+};
